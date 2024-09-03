e46d4aee7cd2a8597c57b1168a1629376e51b6c9
Shalin Okada <shalin.okada@wnd.co.jp>
2024-09-03 15:22:56 +0900

    🔧 Configure pigment css

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import { createTheme } from "@mui/material";
+import { withPigment } from "@pigment-css/nextjs-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+/**
+ * @type {import('@pigment-css/nextjs-plugin').PigmentOptions}
+ */
+const pigmentConfig = {
+  transformLibraries: ["@mui/material"],
+  theme: createTheme(),
+};
+
+export default withPigment(nextConfig, pigmentConfig);
