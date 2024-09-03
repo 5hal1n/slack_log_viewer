0148e03bf8920a9a0d672a49d4919605fcc7c911
Shalin Okada <shalin.okada@wnd.co.jp>
2024-09-03 16:03:18 +0900

    🔥 Remove props from Header's AppBar component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,7 @@ import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 
 const Header = () => {
   return (
-    <AppBar component="div" position="fixed" elevation={1}>
+    <AppBar>
       <Toolbar variant="dense">
         <IconButton></IconButton>
         <Typography variant="h6" component="h1">
