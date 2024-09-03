1c6bac7cdf726179ad5e4ccc043003497d6c8a73
Shalin Okada <shalin.okada@wnd.co.jp>
2024-09-03 15:57:30 +0900

    🏷️ Add sx props to simple html tag

---
+++ src/@types/mui.ts
@@ -0,0 +1,12 @@
+import type { Theme, SxProps } from "@mui/material/styles";
+
+declare global {
+  namespace React {
+    interface HTMLAttributes<T> {
+      sx?: SxProps<Theme>;
+    }
+    interface SVGProps<T> {
+      sx?: SxProps<Theme>;
+    }
+  }
+}
