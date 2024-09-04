8cf3a25e71ac394bd5d059fb3f03eb5daab03c2c
Shalin Okada <shalin.okada@wnd.co.jp>
2024-09-04 13:32:22 +0900

    ✨ Add basic auth

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+const middleware = (req: NextRequest): NextResponse => {
+  if (process.env.NODE_ENV !== "production") return NextResponse.next();
+  const basicAuth = req.headers.get("authorization");
+
+  if (basicAuth) {
+    const [user, pwd] = atob(basicAuth.split(" ")[1]).split(":");
+    if (
+      user === process.env.NEXT_PUBLIC_BASIC_AUTH_USER &&
+      pwd === process.env.NEXT_PUBLIC_BASIC_AUTH_PASSWORD
+    )
+      return NextResponse.next();
+  }
+
+  const url = req.nextUrl;
+  url.pathname = "/api/auth";
+  return NextResponse.rewrite(url);
+};
+
+export { middleware };
