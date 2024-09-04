4f224e7f81d343a728733585f48b79d32c72f08f
Shalin Okada <shalin.okada@wnd.co.jp>
2024-09-04 13:15:03 +0900

    🐛 Fix bug

--- src/components/ChannelList/index.tsx
+++ src/components/ChannelList/index.tsx
@@ -1,13 +1,11 @@
 import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
-import { Channel } from "@/types/channel";
 import Link from "next/link";
+import CHANNELS from "@/data/channels";
 
 const ChannelList = async () => {
-  const res = await fetch("http://localhost:3020/channels");
-  const channels = (await res.json()) as Channel[];
   return (
     <List disablePadding>
-      {channels.map((channel) => (
+      {CHANNELS.map((channel) => (
         <ListItem key={channel.id}>
           <ListItemButton LinkComponent={Link} href={`/${channel.id}`}>
             <ListItemText primary={channel.name} />
